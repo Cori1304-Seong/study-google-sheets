97da3ab41868b6421d1031b0f94240ebc55c8c15
gayathrichelluri <gayathri4040#gmail.com>
2024-09-23 15:14:08 +1000

    connect to google sheet api - via route, via server action

---
+++ src/app/actions/get-gl-data.ts
@@ -0,0 +1,41 @@
+'use server';
+import { google } from 'googleapis';
+
+type TCredential = {
+	type: string;
+	project_id: string;
+	private_key_id: string;
+	private_key: string;
+	client_email: string;
+	client_id: string;
+	universe_domain: string;
+};
+
+export async function getSheetData() {
+	const credential: TCredential = JSON.parse(
+		atob(process.env.GOOGLE_SERVICE_KEY ?? '')
+	);
+
+	const glAuth = await google.auth.getClient({
+		projectId: credential.project_id,
+		credentials: {
+			type: credential.type,
+			project_id: credential.project_id,
+			private_key_id: credential.private_key_id,
+			private_key: credential.private_key,
+			client_email: credential.client_email,
+			client_id: credential.client_id,
+			universe_domain: credential.universe_domain,
+		},
+		scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+	});
+
+	const glSheets = google.sheets({ version: 'v4', auth: glAuth });
+
+	const data = await glSheets.spreadsheets.values.get({
+		spreadsheetId: process.env.SHEET_ID,
+		range: 'A1:J',
+	});
+
+	return { data: data?.data?.values };
+}
--- src/app/actions/posts.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-export const getPosts = async () => {
-	const data = await fetch('https://api.vercel.app/blog');
-	const posts = await data.json();
-
-	return posts;
-};
